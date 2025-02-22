c18a045acff617651093d1843ccba538df32993a
jptolozac <jeanpaultoloza@gmail.com>
2025-02-22 11:40:02 -0500

    Actualizar configuración del sitio para cambiar el dominio a consultas.prosperidadyunion.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://clientes.prosperidadyunion.com',
+  site: 'https://consultas.prosperidadyunion.com',
   base: '/'
 });
