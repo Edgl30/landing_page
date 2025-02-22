842f0da9e33fecf10c1ab011e28af94533c33474
DarkBunnyLaptop <edgarzonl30@gmail.com>
2025-02-21 19:37:16 -0800

    1ra entrega

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import image from '@astrojs/image';
+
 // https://astro.build/config
 export default defineConfig({
+  integrations: [image()],
   vite: {
     plugins: [tailwindcss()],
   },
